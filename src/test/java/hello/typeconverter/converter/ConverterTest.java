151865be494a582e43db9aa75a6f70bf5bbbdd2c
yoonmin-kim <rladbsals23@gmail.com>
2021-08-12 23:13:16 +0900

    feat : 타입 컨버터
    org.springframework.core.convert.converter.Converter 인터페이스를
    implements한 클래스를 구현해서 convert메소드를 오버라이드한다.
    
    String -> Integer
    Integer -> String
    Stirng -> IpPort
    IPort -> String
    
    등.. 여러가지 방식으로 타입 컨버팅이 가능하다.
    스프링에 타입컨버터를 등록하기 위해서 ConversionService를 사용해야한다.

---
+++ src/test/java/hello/typeconverter/converter/ConverterTest.java
@@ -0,0 +1,38 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConverterTest {
+
+    @Test
+    void stringToInteger() {
+        StringToIntegerConverter converter = new StringToIntegerConverter();
+        Integer result = converter.convert("10");
+        assertThat(result).isEqualTo(10);
+    }
+
+    @Test
+    void integerToString() {
+        IntegerToStringConverter converter = new IntegerToStringConverter();
+        String result = converter.convert(10);
+        assertThat(result).isEqualTo("10");
+    }
+
+    @Test
+    void stringToIpPort() {
+        StringToIpPortConverter converter = new StringToIpPortConverter();
+        IpPort result = converter.convert("127.0.0.1:8080");
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+    }
+
+    @Test
+    void ipPortToString() {
+        IpPortToStringConverter converter = new IpPortToStringConverter();
+        String result = converter.convert(new IpPort("127.0.0.1", 8080));
+        assertThat(result).isEqualTo("127.0.0.1:8080");
+    }
+}
